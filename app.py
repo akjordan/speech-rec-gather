308b066e71d86ed4cd22c335ef332a7827f395b0
Andrew <andrew.k.jordan@gmail.com>
2017-05-17 10:00:07 -0700

    trim down app, refactor a bit

--- app.py
+++ app.py
@@ -1,43 +1,36 @@
-from flask import Flask
-from flask import render_template
-from flask import url_for
-from flask import request
-from flask import abort, Flask, request
-
+from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse, Gather
 
-# Declare and configure application
-app = Flask(__name__, static_url_path='/static')
-app.config.from_pyfile('local_settings.py')
+app = Flask(__name__)
 
 # Handle incoming voice call
 @app.route('/incoming', methods=['GET', 'POST'])
 def voice():
     twiml = VoiceResponse()
-    twiml.say("Hello, welcome to Twilio ASR.")
-    gather = Gather(action="/callback",
-     timeout="2",
-     input="speech",
-     partial_result_callback="/partial")
-    gather.say("Please tell us why you're calling.")
-    gather.pause(length="2")
+    gather = Gather(action='/callback',
+     timeout='3',
+     input='speech',
+     partial_result_callback='/partial')
+    gather.say('Welcome to Signal Air, where can we take you?')
     twiml.append(gather)
     return str(twiml)
 
 # Handle POST from <Gather> action read out final result
 @app.route('/callback', methods=['GET', 'POST'])
 def callback():
+    speech_result = request.values.get('SpeechResult', None)
+    print 'SpeechResult: ' + speech_result
     twiml = VoiceResponse()
-    twiml.say("You said" + request.values.get('SpeechResult', None))
+    twiml.say('You said ' + speech_result)
     return str(twiml)
 
-# Handle POST from <Gather> partialResultCallback print intermediate results
+# Handle POST from <Gather> partialResultCallback and print intermediate results
 @app.route('/partial', methods=['GET', 'POST'])
 def partial():
-    print "SequenceNumber: " + request.values.get('SequenceNumber', None)
-    print "UnstableSpeechResult: " + request.values.get('UnstableSpeechResult', None)
-    print "StableSpeechResult: " + request.values.get('StableSpeechResult', None)
-    return "OK"
+    print 'SequenceNumber: ' + request.values.get('SequenceNumber', None)
+    print 'UnstableSpeechResult: ' + request.values.get('UnstableSpeechResult', None)
+    print 'StableSpeechResult: ' + request.values.get('StableSpeechResult', None)
+    return 'OK'
 
 if __name__ == '__main__':
     app.run(port=9393, debug=True)
--- local_settings.py
+++
@@ -1,4 +0,0 @@
-# Begin configuration - configured through environment variables.
-import os
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
-TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
